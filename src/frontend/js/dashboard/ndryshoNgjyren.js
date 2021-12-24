e88ec1c2bda222253f366fd4241cb8ec43c723ca
Albulena <albulena.makolli2@student.uni-pr.edu>
2021-12-24 14:31:16 -0800

    Krijimi i "Rrezo" logos dhe fillimi i ndryshoNgjyren.js

---
+++ src/frontend/js/dashboard/ndryshoNgjyren.js
@@ -0,0 +1,40 @@
+function ndryshoNgjyren() {
+    if (document.documentElement.style.backgroundImage === 'url("../images/dashboardImagePurple.png")') {
+        document.documentElement.style.setProperty("--clr-neon", "");
+        document.documentElement.style.setProperty("--clr-bg", "");
+        document.documentElement.style.setProperty("--clr-neon-light", "");
+        document.documentElement.style.setProperty("--clr-purple", "");
+        document.documentElement.style.setProperty("--clr-neon-qelt", "");
+        document.documentElement.style.setProperty("--clr-neon-mshelt", "");
+        document.documentElement.style.setProperty("--clr-neon-zi", "");
+        document.documentElement.style.setProperty("--clr-coral", "");
+        document.documentElement.style.setProperty("--clr-pink-bardh", "");
+        document.documentElement.style.setProperty("--clr-pink-qelt", "");
+        document.documentElement.style.setProperty("--clr-lekure", "");
+        document.documentElement.style.setProperty("--clr-kuq-qelt", "");
+        document.documentElement.style.setProperty("--clr-sdi", "");
+        document.documentElement.style.setProperty("--background-image-rrezo", "url(../images/dashboardBlue.png)");
+        document.documentElement.style.backgroundImage = "url(../images/dashboardBlue.png)";
+
+
+    } else {
+        document.documentElement.style.setProperty("--clr-neon", "");
+        document.documentElement.style.setProperty("--clr-bg", "");
+        document.documentElement.style.setProperty("--clr-neon-light", "");
+        document.documentElement.style.setProperty("--clr-purple", "");
+        document.documentElement.style.setProperty("--clr-neon-qelt", "");
+        document.documentElement.style.setProperty("--clr-neon-mshelt", "");
+        document.documentElement.style.setProperty("--clr-neon-zi", "");
+        document.documentElement.style.setProperty("--clr-coral", "");
+        document.documentElement.style.setProperty("--clr-pink-bardh", "");
+        document.documentElement.style.setProperty("--clr-pink-qelt", "");
+        document.documentElement.style.setProperty("--clr-lekure", "");
+        document.documentElement.style.setProperty("--clr-kuq-qelt", "");
+        document.documentElement.style.setProperty("--clr-sdi", "");
+        document.documentElement.style.setProperty("--background-image-rrezo", "url(../images/dashboardImagePurple.png)");
+        document.documentElement.style.backgroundImage = "url(../images/dashboardImagePurple.png)";
+
+    }
+
+
+}
